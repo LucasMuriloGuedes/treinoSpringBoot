8cec0bbb50f93936ef92f224357af2f33d5ddf82
Lucas Murilo Guedes Oliveira <lucasmurilo.guedes@gmail.com>
2021-07-24 12:37:38 -0400

    Inserção User

--- src/main/java/com/lucasmurilo/cursomc/resources/UserResource.java
+++ src/main/java/com/lucasmurilo/cursomc/resources/UserResource.java
@@ -3,12 +3,12 @@ package com.lucasmurilo.cursomc.resources;
 import com.lucasmurilo.cursomc.domain.User;
 import com.lucasmurilo.cursomc.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.web.servlet.ServletComponentScan;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
 
 @RestController
@@ -29,4 +29,12 @@ public class UserResource {
         User user = userService.findById(id);
         return ResponseEntity.ok().body(user);
     }
+
+    @PostMapping
+    public ResponseEntity<User> insert(@RequestBody User obj){
+        obj = userService.insert(obj);
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
+        return ResponseEntity.created(uri).body(obj);
+    }
+
 }
